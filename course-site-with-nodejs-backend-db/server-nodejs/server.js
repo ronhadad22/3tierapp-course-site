8b79be2ae2fe9d92c4dc9b21a39ed9ec1d3d89cd
ronbhadad22 <https://www.google.com/support/accounts/bin/answer.py?answer=181692>
2025-10-13 02:35:05 +0300

    test actions

--- course-site-with-nodejs-backend-db/server-nodejs/server.js
+++ course-site-with-nodejs-backend-db/server-nodejs/server.js
@@ -11,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
-
 // Get all courses
 app.get('/api/courses', async (req, res) => {
   const courses = await prisma.course.findMany();
